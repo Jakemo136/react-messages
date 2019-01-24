6b687ce639fee25a76516d4b1304f909b61b2b2a
Jake Mosher <jakemo136@gmail.com>
2019-01-24 10:28:06 -0700

    componentDidMount successful, updating state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import Compose from './components/Compose'
+import Messages from './components/Messages'
+
+const API = 'https://jakemo136-galvanize-message.herokuapp.com/messages'
 
 class App extends Component {
+  
+  state = {
+    messages: [],
+    toggleCompose: false
+  }
+
+  async componentDidMount(){
+    const response = await fetch(API)
+    const responseJson = await response.json()
+    this.setState({messages: responseJson})
+    console.log(this.state.messages)
+  }
+
   render() {
     return (
       <div className="App">
-      <div className="">
-        <Navbar />
-      </div>
-      <div className="container">
-        <Compose />
-        <div>Message display!</div>
-      </div>
+        <div>
+          <Navbar />
+        </div>
+        <div className="container">
+          <Compose />
+          <Messages />
+        </div>
       </div>
     );
   }
---
+++ src/components/Messages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Message from './Message'
+
+const Messages = () => {
+  return(
+    <div>
+      message goes here
+    </div>
+  )
+}
+
+export default Messages
